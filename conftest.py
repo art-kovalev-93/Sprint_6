77aedacff6ed6968675d5e15dd902ecd8d707102
Artem Kovalev <kak-19931993@mail.ru>
2025-12-25 16:07:54 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -1,21 +1,39 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
 def driver():
-    # Указываем адрес Selenium Hub (в нашем случае это localhost:4444)
-    hub_url = "http://localhost:4444/wd/hub"
+    """Фикстура для инициализации и закрытия драйвера Selenium."""
 
-    # Создаем capabilities для Chrome
-    capabilities = DesiredCapabilities.CHROME.copy()
+    # Настройки Chrome
+    chrome_options = Options()
 
-    # Создаем экземпляр удаленного драйвера
-    driver = webdriver.Remote(command_executor=hub_url, desired_capabilities=capabilities)
+    # Обязательные опции для работы в Docker
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    #chrome_options.add_argument('--headless')  # раскомментировать для headless режима
 
-    # Возвращаем драйвер
+    # Дополнительные опции для стабильности
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+
+    # Для Selenium 4.x используем только options
+    driver = webdriver.Remote(
+        command_executor='http://localhost:4444/wd/hub',
+        options=chrome_options
+    )
+
+    # Устанавливаем неявные ожидания
+    driver.implicitly_wait(10)
+
+    # Максимизируем окно браузера
+    driver.maximize_window()
+
     yield driver
 
-    # Закрываем драйвер после теста
+    # Закрываем браузер после теста
     driver.quit()
