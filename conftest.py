d8874e3f5e876b77dcd28ede06de878b2c3c49b1
Artem Kovalev <kak-19931993@mail.ru>
2025-12-25 15:20:00 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -1,13 +1,25 @@
 import pytest
 from selenium import webdriver
 import urls
+from selenium.webdriver.chrome.options import Options
 
 
-
-@pytest.fixture
+@pytest.fixture(scope="session")
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get(urls.base_url)
+    chrome_options = Options()
+
+    # Опции для Docker среды
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--window-size=1920,1080')
+
+
+    driver = webdriver.Remote(
+        command_executor='http://localhost:4444/wd/hub',
+        options=chrome_options
+    )
+    driver.implicitly_wait(10)
+
     yield driver
     driver.quit()
