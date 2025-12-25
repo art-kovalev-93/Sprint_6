8ba287636c8db036a4f7027c92feb2d9156e86cb
Artem Kovalev <kak-19931993@mail.ru>
2025-12-25 16:15:26 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -1,39 +1,34 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
 
 
 @pytest.fixture(scope="function")
 def driver():
-    """Фикстура для инициализации и закрытия драйвера Selenium."""
+    """Фикстура с автоматической установкой ChromeDriver."""
 
-    # Настройки Chrome
     chrome_options = Options()
 
-    # Обязательные опции для работы в Docker
+    # Опции для Docker
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
-    #chrome_options.add_argument('--headless')  # раскомментировать для headless режима
+    chrome_options.add_argument('--window-size=1920,1080')
 
-    # Дополнительные опции для стабильности
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_experimental_option('useAutomationExtension', False)
+    # Используем webdriver-manager для автоматической загрузки драйвера
+    service = ChromeService(ChromeDriverManager().install())
 
-    # Для Selenium 4.x используем только options
-    driver = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
+    driver = webdriver.Chrome(
+        service=service,
         options=chrome_options
     )
 
-    # Устанавливаем неявные ожидания
     driver.implicitly_wait(10)
-
-    # Максимизируем окно браузера
-    driver.maximize_window()
 
     yield driver
 
-    # Закрываем браузер после теста
     driver.quit()
